016414c8d57f25dd64860138a18ebbd92c8501b2
Forte-NaoP <bae9750@gmail.com>
2023-11-03 23:30:09 +0900

    Edit SQLALCHEMY_DATABASE_URL

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./redsec.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./database.sqlite"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
