60d6e6d77a137ec4638f58388794bd8c7625b27e
Forte-NaoP <bae9750@gmail.com>
2023-11-03 23:30:48 +0900

    Remode unused code

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from domain.answer import answer_router
-from domain.question import question_router
 from domain.user import user_router
 from domain.ML_model import ML_model_router
 
@@ -21,8 +19,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-app.include_router(question_router.router)
-app.include_router(answer_router.router)
 app.include_router(user_router.router)
 app.include_router(ML_model_router.router)
