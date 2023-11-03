dd2fc2786274dec3d2c503662f7345582745f647
Forte-NaoP <bae9750@gmail.com>
2023-11-03 23:29:49 +0900

    Add api for model manage

--- domain/ML_model/ML_model_router.py
+++ domain/ML_model/ML_model_router.py
@@ -3,11 +3,14 @@ from fastapi.responses import FileResponse
 from sqlalchemy.orm import Session
 from starlette import status
 from pathlib import Path
+from pathvalidate import sanitize_filename, sanitize_filepath
+
 
 import os
 import uuid
 from typing import List
 from subprocess import Popen, PIPE
+import shutil
 
 from database import get_db
 from domain.user.user_router import get_current_user
@@ -33,6 +36,15 @@ def download_client(db: Session = Depends(get_db),
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="File not found!")
 
+@router.get("/list", response_model=ML_model_schema.ModelList)
+def ml_model_list(db: Session = Depends(get_db),
+                  current_user: User = Depends(get_current_user),
+                  page: int = 0, size: int = 10):
+    total, models = ML_model_crud.get_model_list(db=db, user=current_user, skip=page*size, limit=size)
+    return {
+        'total': total,
+        'models': models
+    }
 
 @router.post("/upload", status_code=status.HTTP_204_NO_CONTENT)
 async def upload_model(db: Session = Depends(get_db),
@@ -47,36 +59,35 @@ async def upload_model(db: Session = Depends(get_db),
 
     exts = list(map(lambda file: os.path.splitext(file.filename)[1], files))
     print(exts)
-    if not ('.key' in exts):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="public key not found!")
 
+    # if not ('.key' in exts):
+    #     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="public key not found!")
+    # if not ('.dat' in exts) or ('.pth' in exts):
+    #     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="model weight file not found!")
+    
     path_prefix = f"{uuid.uuid4()}"
     ML_model_crud.save_model(db=db, user=current_user, name=name, path=path_prefix)
 
-    for file in files:
-        file_extension = os.path.splitext(file.filename)[1]
-        safe_filename = f"{path_prefix}{file_extension}"
+    model_dir_path = os.path.join(user_folder, path_prefix)
+    if not os.path.exists(model_dir_path):
+        os.makedirs(model_dir_path)
 
-        with open(os.path.join(user_folder, safe_filename), "wb") as buffer:
+    for file in files:
+        # file_extension = os.path.splitext(file.filename)[1]
+        # safe_filename = f"{path_prefix}{file_extension}"
+        safe_filename = sanitize_filename(file.filename)
+
+        with open(os.path.join(model_dir_path, safe_filename), "wb") as buffer:
             contents = await file.read()
             buffer.write(contents)
 
+    return
 
-@router.get("/list", response_model=ML_model_schema.ModelList)
-def ml_model_list(db: Session = Depends(get_db),
-                  current_user: User = Depends(get_current_user),
-                  page: int = 0, size: int = 10):
-    total, models = ML_model_crud.get_model_list(db=db, user=current_user, skip=page*size, limit=size)
-    return {
-        'total': total,
-        'models': models
-    }
-
-
-@router.post("/inference/{model_uuid}")
+@router.post("/{model_uuid}/inference")
 async def inference_model(model_uuid: str,
                           db: Session = Depends(get_db),
                           current_user: User = Depends(get_current_user),
+                          name : str = Form(...),
                           file: UploadFile = File(...)):
 
     model = ML_model_crud.get_model_by_uuid(db=db, uuid=model_uuid)
@@ -86,23 +97,91 @@ async def inference_model(model_uuid: str,
     user_folder = os.path.join(user_prefix, current_user.username)
     model_path = os.path.join(user_folder, model.ML_model_uuid)
 
-    if not os.path.exists(f"{model_path}.dat"):
+    if not os.path.isdir(model_path):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Model file not found!")
+    
+    image_dir = os.path.join(model_path, 'images')
+    if not os.path.exists(image_dir):
+        os.makedirs(image_dir)
+    
+    safe_filename = f"{uuid.uuid4()}"
 
-    with open(f"{model_path}.img", "wb") as buffer:
+    with open(os.path.join(image_dir, safe_filename), "wb") as buffer:
         contents = await file.read()
         buffer.write(contents)
 
-    env = os.environ.copy()
-    env["model_path"] = f"{model_prefix}{model_path}.dat"
-    env["key_path"] = f"{model_prefix}{model_path}.key"
-    env["image_path"] = f"{model_prefix}{model_path}.img"
-    env["output_path"] = f"{model_prefix}{model_path}.res"
+    ML_model_crud.save_file(db=db, model=model, description=name, file_name=safe_filename)
 
-    print(env["model_path"])
-    process = Popen(["make", "cpu-encrypt"], stdout=PIPE, stderr=PIPE, env=env, cwd="./redsec")
-    stdout, stderr = process.communicate()
+    print(model_path)
+    
+    return
 
-    return {"output": "Inference Processing Now!"}
+    # if not os.path.exists(f"{model_path}.dat"):
+    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Model file not found!")
+
+    # with open(f"{model_path}.img", "wb") as buffer:
+    #     contents = await file.read()
+    #     buffer.write(contents)
+
+    # env = os.environ.copy()
+    # env["model_path"] = f"{model_prefix}{model_path}.dat"
+    # env["key_path"] = f"{model_prefix}{model_path}.key"
+    # env["image_path"] = f"{model_prefix}{model_path}.img"
+    # env["output_path"] = f"{model_prefix}{model_path}.res"
+
+    # print(env["model_path"])
+    # process = Popen(["make", "cpu-encrypt"], stdout=PIPE, stderr=PIPE, env=env, cwd="./redsec")
+    # stdout, stderr = process.communicate()
+
+    # time_elapsed = list(filter(lambda x: x.startswith("Inference"), stdout.decode().split("\n")))
+    # print(time_elapsed)
+    # return {"result": time_elapsed}
 
 
+@router.get("/{model_uuid}/history", response_model=ML_model_schema.ModelHistoryList)
+async def get_model_history(model_uuid: str,
+                            db: Session = Depends(get_db),
+                            current_user: User = Depends(get_current_user)):
+
+    model = ML_model_crud.get_model_by_uuid(db=db, uuid=model_uuid)
+    if model is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Model not Registered!")
+
+    total, history = ML_model_crud.get_model_history(db=db, model=model)
+    return {
+        'total': total,
+        'history': history
+    }
+
+
+@router.get("/{model_uuid}/{file_name}/download")
+def download_inference_data(model_uuid: str, file_name: str,
+                            db: Session = Depends(get_db),
+                            current_user: User = Depends(get_current_user)):
+    
+    file_path = Path(f"./users/{current_user.username}/{model_uuid}/images/{file_name}")
+    print('asd')
+    if file_path.is_file():
+        return FileResponse(file_path, media_type="application/zip", filename=file_path.name)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="File not found!")
+
+
+@router.delete("/{model_uuid}/delete")
+def delete_model(model_uuid: str,
+                 db: Session = Depends(get_db),
+                 current_user: User = Depends(get_current_user)):
+
+    model = ML_model_crud.get_model_by_uuid(db=db, uuid=model_uuid)
+    if model is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Model not Registered!")
+
+    user_folder = os.path.join(user_prefix, current_user.username)
+    model_path = os.path.join(user_folder, model.ML_model_uuid)
+
+    if os.path.isdir(model_path):
+        shutil.rmtree(model_path)
+        ML_model_crud.delete_model(db=db, model=model)
+        return {"result": "삭제되었습니다."}
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Model file not found!")
