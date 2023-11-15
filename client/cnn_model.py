81f507c149694c4be8d75861b3b3fd1b85541ef0
Forte-NaoP <bae9750@gmail.com>
2023-11-15 16:38:26 +0900

    Edit model template code

--- client/cnn_model.py
+++ client/cnn_model.py
@@ -1,6 +1,15 @@
 import torch
 
 model_file = 'your_model_file.pth'
+# data_size must be larger than maximum out_feature of your model fc layers
+data_size = 1080
+
+
+# must use this function as activation function of your model
+def apporximate_relu(x):
+    x = 0.117071 * x ** 2 + 0.5 * x + 0.375373
+    return x
+
 
 class CNN(torch.nn.Module):
     def __init__(self):
