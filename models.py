3be86611b1e1bb017f2ace3ccb7182cf2b8df316
Forte-NaoP <bae9750@gmail.com>
2023-11-16 19:15:23 +0900

    Delete unused table

--- models.py
+++ models.py
@@ -4,32 +4,6 @@ from sqlalchemy.orm import relationship, backref
 from database import Base
 
 
-class Question(Base):
-    __tablename__ = "question"
-
-    id = Column(Integer, primary_key=True)
-    subject = Column(String, nullable=False)
-    content = Column(Text, nullable=False)
-    create_date = Column(DateTime, nullable=False)
-    user_id = Column(Integer, ForeignKey("user.id"), nullable=True)
-    user = relationship("User", backref="question_users")
-    modify_date = Column(DateTime, nullable=True)
-    answers = relationship("Answer", back_populates="question", cascade="all, delete, delete-orphan")
-
-
-class Answer(Base):
-    __tablename__ = "answer"
-
-    id = Column(Integer, primary_key=True)
-    content = Column(Text, nullable=False)
-    create_date = Column(DateTime, nullable=False)
-    question_id = Column(Integer, ForeignKey("question.id"))
-    question = relationship("Question", back_populates="answers")
-    user_id = Column(Integer, ForeignKey("user.id"), nullable=True)
-    user = relationship("User", backref="answer_users")
-    modify_date = Column(DateTime, nullable=True)
-
-
 class User(Base):
     __tablename__ = "user"
 
